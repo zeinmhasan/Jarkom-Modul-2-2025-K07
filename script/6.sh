662878c245e3ae27946362ce29efed13786fc70e
AnandaFitriww <anandawibowo411@gmail.com>
2025-10-13 20:19:02 +0700

    Update 6.sh

--- script/6.sh
+++ script/6.sh
@@ -1,14 +1,14 @@
 # di valmar
 # ambil serial dari ns1
-dig @10.67.3.3 zein.com SOA +noall +answer | awk '{print $7}'
+dig @10.67.3.3 k07.com SOA +noall +answer | awk '{print $7}'
 # ambil serial dari ns2
-dig @10.67.3.4 zein.com SOA +noall +answer | awk '{print $7}'
+dig @10.67.3.4 k07.com SOA +noall +answer | awk '{print $7}'
 
 nano /etc/bind/named.conf.local
-zone "zein.com" {
+zone "k07.com" {
     type slave;
     masters { 10.67.3.3; };                 // Tirion (ns1)
-    file "/var/cache/bind/zein.com.slave";
+    file "/var/cache/bind/k07.com.slave";
     allow-notify { 10.67.3.3; };
     allow-transfer { 127.0.0.1; 10.67.3.0/24; };  // IZINKAN AXFR (lab)
 };
@@ -17,6 +17,6 @@ named-checkconf
 kill -HUP $(cat /run/named/named.pid) 2>/dev/null || \
 { pkill named; /usr/sbin/named -4 -u bind -c /etc/bind/named.conf; }
 
-dig @127.0.0.1 zein.com AXFR
+dig @127.0.0.1 k07.com AXFR
 # atau
-dig @10.67.3.4 zein.com AXFR
+dig @10.67.3.4 k07.com AXFR
